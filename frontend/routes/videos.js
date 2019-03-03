8133c0169eebfd48d8cf53699f7e644296da23d2
Zak Graber <zakgraber@gmail.com>
2019-03-02 16:40:28 -0800

    Started search bar form
    stuff

--- frontend/routes/videos.js
+++ frontend/routes/videos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const ejs = require('ejs');
+const ejs = require("ejs");
 
 let ledger_ip = 'IP GOES HERE';
 
@@ -15,13 +15,16 @@ const handleError = (err, res) => {
   };
 
 
-app.get('/videos/:id', (req, res) => {
-    let vid_id = req.params.id;
+
+//Gets a video
+router.post('/', (req, res) => {
+    let vid_id = req.body.id;
+    console.log(req.body);
     const payload = {
         id: vid_id
     }
 
-    fetch(ledger_ip, {
+    fetch(ledger_ip + '/LedgerReq', {
         method: 'POST',
         body: payload
     })
@@ -34,4 +37,26 @@ app.get('/videos/:id', (req, res) => {
     res.render('stream');
 });
 
-app.post('/videos/upload')
+
+//Uploads the video
+router.post('/upload', (req, res) => {
+    //Get the stream node's ip to upload the video to
+    //Upload the video to that node
+    let ip;
+    
+    //Should send a post request to ledger node which should update its database
+    //and send me back an array of ip addresses of available nodes
+    fetch(ledger_ip + '/GetAddress', {
+        method: 'POST',
+    })
+    .then(function(response) {
+        return(response.json());
+    })
+    .then(function(myJSON) {
+        console.log(myJSON.ip);
+    });
+    res.render('stream');
+
+});
+
+module.exports = router;
--- frontend/server.js
+++ frontend/server.js
@@ -2,17 +2,19 @@ var express = require('express');
 var fetch = require('node-fetch');
 var ejs = require('ejs')
 var app = express();
+var bodyParser = require('body-parser');
 
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
 
 var videosRouter = require('./routes/videos');
 
 app.use('/videos',videosRouter);
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('index');
 });
 
 let port = process.env.PORT;
