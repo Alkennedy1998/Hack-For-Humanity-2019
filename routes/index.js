8b765213ac64ced85d59a8068725a408934c696c
Donovan Allen <dallen@sovrn.com>
2019-03-03 00:58:46 -0700

    Add stat endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const ejs = require('ejs');
+const fs = require('fs');
 
+var aNewPath = "./videos/"
+
+const maxCount = 35;
 
 /* GET home page. */
-
-
 router.get('/', (req, res) => res.render('index.ejs'));
+
+router.get('/stat', (req, res) => {
+  const vids = fs.readdirSync(aNewPath);
+  res.send({
+    used: vids.length,
+    total: maxCount,
+    videos: vids.map((e) => e.replace(".mp4", ""))
+  });
+});
 
 /*
 router.get('/', function(req, res, next) {
