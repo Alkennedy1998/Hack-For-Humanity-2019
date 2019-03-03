47f7e3764ce9d1567ec9538c66fc2de5e4263388
williamman56 <henrionwill@gmail.com>
2019-03-02 17:06:37 -0800

    smol comment

--- routes/database.js
+++ routes/database.js
@@ -2,7 +2,9 @@
  
  function parseJson(data, id){
 	let jsonArr = JSON.parse(data);
+	//console.log(id);
 	for (i = 0; i < jsonArr.length; i++){
+		//console.log(jsonArr[i].id);
 		if(jsonArr[i].id === id)
 			return jsonArr[i].ipLoc;
 	}
@@ -11,7 +13,8 @@
 
 const access = {
 	getIp: (id) => {
-		const file = './data/fileData.json';
+		//console.log(id);
+		const file = './routes/data/fileData.json';
 		let raw = fs.readFileSync(file);
 		let ip = parseJson(raw, id);
 		return ip;
--- routes/ledger.js
+++ routes/ledger.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var access = require('./ledger/database.js');
+var access = require('./database.js');
 
 console.log("Made it into ledger");
 
@@ -8,11 +8,12 @@ router.post('/', function(req,res,next){
     
     //get fileID from body of the request
     var fileID = req.body.id;
-    console.log('FileID Recieved');
-
+    //console.log(req.body);
+	
     var ip = access.getIp(fileID);
-
-    res.send(ip);
+	
+	console.log(ip);
+    res.status(204).send(ip[0]);
 
 });
 module.exports = router;
