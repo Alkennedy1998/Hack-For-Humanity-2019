d4851bd4ed70284408b1f20632980631c8c27bd4
cduncan <cduncan@scu.edu>
2019-03-02 17:33:01 -0800

    post for AddNode

--- routes/ledger.js
+++ routes/ledger.js
@@ -4,6 +4,21 @@ var access = require('./database.js');
 
 console.log("Made it into ledger");
 
+router.post('/AddNode', function(req,res,next){
+    //need to get the IP from new SN host, which should have sent in request
+    var SN_IP = req.body;
+    //response will recieve IP and add to each ledger IP array
+
+    console.log(req.body);
+
+    if(access.updateStorageNode(SN_IP, 0) !== -1)
+        res.status(204).send();
+    else
+        res.status(404).send();
+
+});
+
+
 router.post('/', function(req,res,next){
     
     //get fileID from body of the request
@@ -11,9 +26,9 @@ router.post('/', function(req,res,next){
     //console.log(req.body);
 	
     var ip = access.getIp(fileID);
-	
-	console.log(ip);
-    res.status(204).send(ip[0]);
+    
+	//console.log(ip);
+    res.status(204).send(ip);
 
 });
 module.exports = router;
