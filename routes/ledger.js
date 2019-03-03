4067d73af27c46f178167728be1ed2cbd0a372ce
cduncan <cduncan@scu.edu>
2019-03-02 16:45:41 -0800

    update ledger.js

--- routes/ledger.js
+++ routes/ledger.js
@@ -1,35 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var access = require('./ledger/database.js');
 
 console.log("Made it into ledger");
-//array of submissions, each number represents ID for now
-//{ID, tag, IP}
-/*
-var files = [
-    {ID:"1",name: "1",tag:"music",descritpion: "Music Video",IP:["10"]},
-    {ID:"2",name: "2",tag:"cooking",description: "Cooking Video",IP:["20,40"]},
-    {ID:"3",name: "3",tag:"coding",description: "Coding Master Race Video",IP:["30,50,70"]}
-];
-*/
 
-router.get('/filedata/:id', function(req,res,next){
-    //get fileID from body of the request
-    var fileID = req.params.id;
-    console.log('FileID Recieved');
-    let ip = access.getIp(fileID);
+router.post('/', function(req,res,next){
     
-    /*
-    //loop through the list of submissions and find ID that matches requested ID
-    for(var i = 0; i<files.length; i++) {
-        if(fileID == files[i].ID)
-            break;
-    }
+    //get fileID from body of the request
+    var fileID = req.body.id;
+    console.log('FileID Recieved');
 
-    //send IP correleating to specific ID back to user
-    var ip = files[i].IP;
-    console.log(ip);
-    */
+    var ip = access.getIp(fileID);
+
     res.send(ip);
 
 });
