630a84699cb58963af4976adb9cab19223969bdc
Alexander Kennedy <rexer1998@gmail.com>
2019-10-16 01:04:50 +0100

    Removed test programs from master

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = require("./app.js")
-
-let port = process.env.PORT;
-if(port == null || port == ""){
-    port = 80;
-}
-
-app.listen(port, () => console.log(`Image downloader listening on port ${port}!`))
